e0fed58391096864468b6715acff07f2dafa1494
Fischer <fischerrobson@gmail.com>
2021-09-01 23:24:59 -0300

    validacoes do form de envio de imagens

--- src/components/Form/FormAddImage.tsx
+++ src/components/Form/FormAddImage.tsx
@@ -1,30 +1,50 @@
-import { Box, Button, Stack, useToast } from '@chakra-ui/react';
-import { useForm } from 'react-hook-form';
-import { useState } from 'react';
-import { useMutation, useQueryClient } from 'react-query';
+import { Box, Button, Stack, useToast } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "react-query";
 
-import { api } from '../../services/api';
-import { FileInput } from '../Input/FileInput';
-import { TextInput } from '../Input/TextInput';
+import { api } from "../../services/api";
+import { FileInput } from "../Input/FileInput";
+import { TextInput } from "../Input/TextInput";
 
 interface FormAddImageProps {
   closeModal: () => void;
 }
 
 export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
-  const [imageUrl, setImageUrl] = useState('');
-  const [localImageUrl, setLocalImageUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState("");
+  const [localImageUrl, setLocalImageUrl] = useState("");
   const toast = useToast();
 
   const formValidations = {
     image: {
-      // TODO REQUIRED, LESS THAN 10 MB AND ACCEPTED FORMATS VALIDATIONS
+      required: "Arquivo obrigatório",
+      validate: {
+        lessThan10MB: fileList =>
+          fileList[0].size < 10485760 || "O arquivo deve ser menor que 10MB",
+        acceptedFormats: fileList =>
+          /(?:([^:/?#]+):)?(?:([^/?#]*))?([^?#](?:jpeg|gif|png))(?:\?([^#]*))?(?:#(.*))?/g.test(
+            fileList[0].type
+          ) || "Somente são aceitos arquivos PNG, JPEG e GIF",
+      }
     },
     title: {
-      // TODO REQUIRED, MIN AND MAX LENGTH VALIDATIONS
+      required: "Título obrigatório",
+      minLength: {
+        value: 2,
+        message: "Mínimo de 2 caracteres"
+      },
+      maxLength: {
+        value: 20,
+        message: "Máximo de 20 caracteres"
+      }
     },
     description: {
-      // TODO REQUIRED, MAX LENGTH VALIDATIONS
+      required: "Descrição obrigatória",
+      maxLength: {
+        value: 65,
+        message: "Máximo de 65 caracteres"
+      }
     },
   };
 
@@ -48,7 +68,16 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
 
   const onSubmit = async (data: Record<string, unknown>): Promise<void> => {
     try {
-      // TODO SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
+      if (!imageUrl) {
+        toast({
+          title: "Imagem não adicionada",
+          description:
+            "É preciso adicionar e aguardar o upload de uma imagem antes de realizar o cadastro.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
       // TODO EXECUTE ASYNC MUTATION
       // TODO SHOW SUCCESS TOAST
     } catch {
@@ -67,20 +96,20 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
           setLocalImageUrl={setLocalImageUrl}
           setError={setError}
           trigger={trigger}
-          // TODO SEND IMAGE ERRORS
-          // TODO REGISTER IMAGE INPUT WITH VALIDATIONS
+          error={errors.image}
+          {...register("image", formValidations.image)}
         />
 
         <TextInput
           placeholder="Título da imagem..."
-          // TODO SEND TITLE ERRORS
-          // TODO REGISTER TITLE INPUT WITH VALIDATIONS
+          error={errors.title}
+          {...register("title", formValidations.title)}
         />
 
         <TextInput
           placeholder="Descrição da imagem..."
-          // TODO SEND DESCRIPTION ERRORS
-          // TODO REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          error={errors.description}
+          {...register("description", formValidations.description)}
         />
       </Stack>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,31 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Card {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+type IInfiniteQueryResponse = {
+  after: number | null;
+  data: Card[];
+};
+
 export default function Home(): JSX.Element {
+
+  const getPage = async ({ pageParam = null }): Promise<IInfiniteQueryResponse> => {
+    const { data } = await api.get("/api/images", {
+      params: {
+        after: pageParam
+      }
+    });
+
+    return data;
+  };
+
   const {
     data,
     isLoading,
@@ -18,14 +42,15 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    getPage,
+    {
+      getNextPageParam: (lastPage: { after: number }) => lastPage.after,
+    }
   );
 
-  const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
-  }, [data]);
+  // const formattedData = useMemo(() => {
+  //   // TODO FORMAT AND FLAT DATA ARRAY
+  // }, [data]);
 
   // TODO RENDER LOADING SCREEN
 
@@ -36,7 +61,7 @@ export default function Home(): JSX.Element {
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
+        {/* <CardList cards={formattedData} /> */}
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
       </Box>
     </>
